d8dedf6286e3fef6423f08c47af5a6220b95c8dc
vaibhav10301 <agrawal10301@gmail.com>
2016-03-25 14:31:57 +0530

    CRUD Operation done\

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 resources :articles
   # You can have the root of your site routed with "root"
    root 'articles#index'
-
+   get 'articles/destroy/:id', to:'articles#destroy', as:'articles_destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
