04087283971a1349b6bf81f5343f7371b264aa08
vaibhav10301 <agrawal10301@gmail.com>
2016-03-25 17:18:47 +0530

    After deleting of comment will not go to home page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to articles_path
+    redirect_to article_path(@article)
   end
  
   private
